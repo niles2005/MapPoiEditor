4b5d0e65e0647f5bcacef69ed36113869f3f54ef
NieLei <niles2005@sina.com>
2018-11-07 10:36:30 +0800

    update

--- tomcat/compile.bat
+++
@@ -1,1 +0,0 @@
-javac -Djava.ext.dirs="C:\apache-tomcat-8.0.22\lib;C:\apache-tomcat-8.0.22\bin;." MyTomcat.java
--- tomcat/run.bat
+++
@@ -1,1 +0,0 @@
-java -Xmx300M -Djava.ext.dirs="C:\mysoft\apache-tomcat-8.0.22\lib;C:\mysoft\apache-tomcat-8.0.22\bin;." MyTomcat
